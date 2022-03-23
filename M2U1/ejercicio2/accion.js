5f60d26698d9afbae9840588b0aac2cb48c4569a
Franco Rizzi <rizzi.franco90@gmail.com>
2022-03-23 14:16:10 -0300

    cambio de colores

---
+++ M2U1/ejercicio2/accion.js
@@ -0,0 +1,7 @@
+var texto = document.getElementById('texto');
+var contar = document.getElementById('contar');
+
+texto.addEventListener('keyup', function(){
+    contar.innerText = texto.value.length;
+
+})
