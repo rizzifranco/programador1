1a2f1999936ca63489795f4db7af6f370f4448c5
Franco Rizzi <rizzi.franco90@gmail.com>
2022-06-08 20:19:58 -0300

    error_eliminar

--- M4U4/routes/admin/novedades.js
+++ M4U4/routes/admin/novedades.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var usuariosModel = require('../../models/usuariosModel');
+var novedadesModel = require('../../models/novedadesModel');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
   res.render('admin/novedades',{
       layout:'admin/layout',
-      nombre: req.session.nombre
+      nombre: req.session.nombre,
+      novedades
   } );
+});
+
+router.get('/eliminar/:id', async (req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
 });
 
 module.exports = router;
